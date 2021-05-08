ab3f598096a05d303f56274c31bb23494bb14fca
Sarat Chandra <me@saratchandra.in>
2021-05-08 15:38:40 +0530

    fix: Fix api in frontend.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ export default async function sendSub(pincode, age, token) {
     try {
         const response = await axios.post('/api/v1/subscribe', {
             pincode: pincode,
-            age_limit: age,
+            age: age,
             token: token,
         })
 
